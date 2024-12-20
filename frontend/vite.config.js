8b7d44136bc4666dbf1475e4c435b5de883faf9c
saurabhpaliwal55 <75921538+saurabhpaliwal55@users.noreply.github.com>
2024-12-20 21:52:53 +0530

    proxy updated to the deployed port

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server:{
     proxy:{
-      '/api':"http://localhost:4000"
+      // '/api':"http://localhost:4000"
+      '/api':"http://13.126.68.136:4000"
     }
   }
 })
